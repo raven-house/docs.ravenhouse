d380456cc4b59159ec894bedf49651c0336cdb8a
Yash Mittal <yashmittalmm@gmail.com>
2025-03-24 12:59:26 +0530

    chore: Update links

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Raven House</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/gCPZhWBYQH',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/satyambnsal/docs.ravenhouse',
   footer: {
     text: 'Nextra Docs Template',
   },
