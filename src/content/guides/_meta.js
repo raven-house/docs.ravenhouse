27564acaf194d9eae30911b9f50be6a927ca765c
Yash Mittal <yashmittalmm@gmail.com>
2025-08-20 15:39:02 +0530

    chore: Improve small details of terminilogy

--- src/content/guides/_meta.js
+++ src/content/guides/_meta.js
@@ -1,7 +1,7 @@
 export default {
   "get-started": "Get Started",
-  "launch-nft-collection": "Creating NFT Collection",
-  "launch-voucher-collection": "Creating Voucher Collection",
+  "launch-nft-collection": "Launch NFT Collection",
+  "launch-voucher-collection": "Launch Voucher Collection",
   "whitelist-management": "Whitelist Management",
   "nfts": "Minting NFTs",
   "discord": "Discord Integration",
